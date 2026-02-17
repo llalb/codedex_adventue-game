a5c31bbf1cdc687fbc0eb2b3abb6c107d1dd844a
Luis Albrecht <luis.albrecht66@gmail.com>
2026-02-17 22:01:00 +0100

    Scanner: Would you like to play?
    Including Syntax with loading sequence and loop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to my game! ");
+
+        boolean validInput = false;
+        while (!validInput) {
+            System.out.println("Would you like to play? (y/n) ");
+            char play = scan.next().charAt(0);
+            if (play == 'y') {
+                validInput = true;
+                System.out.print("Loading: ");
+                for (int i = 0; i < 25; i++) {
+                    System.out.print(">");
+                    Thread.sleep(50);
+                }
+            } else if (play == 'n') {
+                System.out.println("See you another time!");
+                validInput = true;
+            } else {
+                System.out.println("Error: invalid input! try y/n");
+                validInput = false;
+            }
+        }
     }
 }
+
